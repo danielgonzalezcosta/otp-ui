6ee2c4d20a7d8b3d2f689d240916d8702c1a63a7
Zsombor Welker <zsombor.welker@realcity.io>
2023-05-23 22:26:53 +0200

    Add the-line-overlay package
    This allows for:
     - showing The Line modules on the map
     - showing stations/stops on the map

---
+++ packages/the-line-overlay/src/deck-gl-mapbox-overlay.tsx
@@ -0,0 +1,52 @@
+import { MapboxOverlay, MapboxOverlayProps } from "@deck.gl/mapbox/typed";
+import { ViewStateChangeParameters } from "@deck.gl/core/typed/controllers/controller";
+
+type Map = any;
+
+export interface DeckGlMapboxOverlayProps extends MapboxOverlayProps {
+  alwaysShow?: boolean;
+  interleaved?: boolean;
+  visible?: boolean;
+}
+
+export class DeckGlMapboxOverlay extends MapboxOverlay {
+  private map: Map;
+
+  private viewState?: ViewStateChangeParameters;
+
+  private readonly onViewStateChange?: (
+    params: ViewStateChangeParameters & { viewId: string }
+  ) => any;
+
+  constructor(props: DeckGlMapboxOverlayProps) {
+    super(props);
+    this.onViewStateChange = props.onViewStateChange;
+  }
+
+  onAdd(map: Map): HTMLDivElement {
+    this.map = map;
+    map.on("render", this.updateViewState);
+    return super.onAdd(map);
+  }
+
+  onRemove(): void {
+    const map = this.map;
+    map.off("render", this.updateViewState);
+    super.onRemove();
+  }
+
+  updateViewState = (): void => {
+    const prefix = "_";
+    const viewState = this.map[`${prefix}_deck`].props.viewState;
+    if (this.onViewStateChange && viewState) {
+      const newViewState = {
+        viewState,
+        oldViewState: this.viewState,
+        interactionState: {},
+        viewId: ""
+      };
+      this.viewState = newViewState;
+      this.onViewStateChange(newViewState);
+    }
+  };
+}
---
+++ packages/the-line-overlay/src/index.tsx
@@ -0,0 +1,329 @@
+import EntityPopup from "@opentripplanner/map-popup";
+import { MapLocationActionArg } from "@opentripplanner/types";
+// eslint-disable-next-line prettier/prettier
+import React, { useCallback, useState } from "react"
+import { Popup, useControl } from "react-map-gl";
+import { GeoJsonLayer } from "@deck.gl/layers/typed";
+import { MVTLayer } from "@deck.gl/geo-layers/typed";
+import {
+  DeckGlMapboxOverlay,
+  DeckGlMapboxOverlayProps
+} from "./deck-gl-mapbox-overlay";
+
+function DeckGLOverlay(props: DeckGlMapboxOverlayProps) {
+  const overlay = useControl<any>(() => new DeckGlMapboxOverlay(props));
+  overlay.setProps(props);
+  return null;
+}
+
+const iconAtlas = `
+<svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 128 64"
+        height="64px"
+        width="128px"
+>
+    <circle cx="32" cy="32" r="16" fill="#FFFFFF" stroke="#FFFFFF" stroke-width="32" stroke-opacity="0.5"/>
+
+    <circle cx="85" cy="21" r="16" fill="none" stroke="#FFFFFF" stroke-opacity="0.5" stroke-width="10"/>
+    <circle cx="85" cy="21" r="6" fill="#FFFFFF" stroke="none"/>
+
+    <circle cx="72" cy="56" r="7" stroke="#FFFFFF" stroke-width="2" fill="none"/>
+</svg>
+`;
+
+const iconMapping = {
+  "super station": {
+    x: 0,
+    y: 0,
+    width: 64,
+    height: 64
+  },
+  station: {
+    x: 64,
+    y: 0,
+    width: 42,
+    height: 42
+  },
+  stop: {
+    x: 64,
+    y: 48,
+    width: 16,
+    height: 16
+  }
+};
+
+export default function TheLineOverlay({
+  id,
+  setLocation,
+  setViewedStop,
+  tilesBaseUrl,
+  otp2Layers,
+  dataUrl,
+  showStopsAndStations,
+  showTheLine,
+  alwaysShow,
+  visible
+}: {
+  id: string;
+  name?: string;
+  tilesBaseUrl: string;
+  otp2Layers: string[];
+  dataUrl: string;
+  showStopsAndStations?: boolean;
+  showTheLine?: boolean;
+  /**
+   * A method fired when a stop is selected as from or to in the default popup. If this method
+   * is not passed, the from/to buttons will not be shown.
+   */
+  setLocation?: (location: MapLocationActionArg) => void;
+  /**
+   * A method fired when the stop viewer is opened in the default popup. If this method is
+   * not passed, the stop viewer link will not be shown.
+   */
+  setViewedStop?: ({ stopId }: { stopId: string }) => void;
+  alwaysShow?: boolean;
+  visible?: boolean;
+}): JSX.Element {
+  const [clickedEntity, setClickedEntity] = useState<any>(null);
+  const [farOut, setFarOut] = useState<boolean>(false);
+  const [fromAbove, setFromAbove] = useState<boolean>(false);
+
+  const tileUrl = `${tilesBaseUrl}/${otp2Layers.join(",")}/tilejson.json`;
+
+  const layers = [];
+
+  if (showStopsAndStations) {
+    layers.push(
+      new MVTLayer({
+        id: `${id}-tiles-bg` as string,
+        data: tileUrl,
+
+        minZoom: 6,
+        maxZoom: 19,
+        pickable: true,
+
+        pointType: "circle",
+
+        stroked: true,
+        filled: true,
+        getFillColor: [0, 0, 0, 255],
+        pointBillboard: false,
+        pointRadiusUnits: "pixels",
+        uniqueIdProperty: "gtfsId",
+
+        autoHighlight: true,
+        highlightColor: [100, 0, 0, 255],
+        getPointRadius: feature => {
+          if (feature.properties.stationLevel >= 2) {
+            return iconMapping["super station"].height / 2;
+          }
+          if (feature.properties.stationLevel >= 1) {
+            return iconMapping.station.height / 2;
+          }
+          return iconMapping.stop.height / 2;
+        },
+
+        getText: feature => feature.properties.name
+      })
+    );
+    layers.push(
+      new MVTLayer({
+        id: `${id}-tiles-fg` as string,
+        data: tileUrl,
+
+        minZoom: 6,
+        maxZoom: 19,
+        pickable: false,
+
+        pointType: "icon",
+
+        visible: true,
+        iconBillboard: false,
+        iconAtlas: `data:image/svg+xml,${encodeURIComponent(iconAtlas)}`,
+        iconMapping,
+        iconSizeUnits: "pixels",
+        iconSizeScale: 1,
+        iconSizeMinPixels: 10,
+        iconSizeMaxPixels: 100,
+        getIconSize: feature => {
+          if (feature.properties.stationLevel >= 2) {
+            return iconMapping["super station"].height;
+          }
+          if (feature.properties.stationLevel >= 1) {
+            return iconMapping.station.height;
+          }
+          return iconMapping.stop.height;
+        },
+        getIcon: feature => {
+          if (feature.properties.stationLevel >= 2) {
+            return "super station";
+          }
+          if (feature.properties.stationLevel >= 1) {
+            return "station";
+          }
+          return "stop";
+        }
+      })
+    );
+  }
+
+  if (showTheLine) {
+    layers.push(
+      new GeoJsonLayer({
+        id: `${id}-geojson-vertibase` as string,
+        data: `${dataUrl}?v=vertibase-${fromAbove}` as string,
+        stroked: true,
+        filled: true,
+        pickable: true,
+        visible: !farOut,
+        pointType: "circle",
+        getPointRadius: 50,
+        getLineColor: [0, 0, 0],
+        getLineWidth: 4,
+        getFillColor: [248, 231, 28, 128],
+        autoHighlight: true,
+        highlightColor: [100, 0, 0, 128],
+        onDataLoad: (value: any) => {
+          value.features = value.features.filter(
+            v => v.properties.point === "vertibase"
+          );
+
+          if (fromAbove) {
+            value.features.forEach(feature => {
+              delete feature.geometry.coordinates[3];
+            });
+          }
+        }
+      }),
+      new GeoJsonLayer({
+        id: `${id}-geojson-level` as string,
+        data: `${dataUrl}?v=2` as string,
+        stroked: true,
+        filled: true,
+        pickable: true,
+        visible: !farOut && !fromAbove,
+        getLineColor: [0, 0, 0, 128],
+        getLineWidth: 4,
+        getFillColor: [0, 0, 0, 0],
+        autoHighlight: true,
+        highlightColor: [100, 0, 0, 128],
+        onDataLoad: (value: any) => {
+          value.features = value.features.filter(
+            v => v.properties.module && v.properties.level
+          );
+        }
+      }),
+      new GeoJsonLayer({
+        id: `${id}-geojson-module-outline` as string,
+        data: `${dataUrl}?v=1.1` as string,
+        stroked: fromAbove && !farOut,
+        extruded: !fromAbove,
+        filled: farOut || fromAbove,
+        wireframe: !fromAbove,
+        pickable: farOut || fromAbove,
+        autoHighlight: true,
+        highlightColor: [100, 0, 0, 255],
+        lineWidthUnits: "pixels",
+        getElevation: f => (fromAbove ? 0 : f.properties.height),
+        getLineColor: [0, 0, 0, 196],
+        getLineWidth: 3,
+        getFillColor: fromAbove ? [200, 200, 200, 196] : [220, 220, 220, 64],
+        onDataLoad: (value: any) => {
+          value.features = value.features.filter(
+            v => v.properties.module && !v.properties.level
+          );
+        }
+      }),
+      new GeoJsonLayer({
+        id: `${id}-geojson-building-fill` as string,
+        data: `${dataUrl}?v=1` as string,
+        stroked: false,
+        extruded: true,
+        filled: true,
+        wireframe: false,
+        getLineColor: [0, 0, 0, 16],
+        getLineWidth: 4,
+        pickable: false,
+        visible: !farOut && !fromAbove,
+        getElevation: f => f.properties.height,
+        getFillColor: [225, 225, 225, 64],
+        onDataLoad: (value: any) => {
+          value.features = value.features.filter(v => v.properties.building);
+        }
+      })
+    );
+  }
+
+  const onClick = useCallback(event => {
+    if (event && event.object) {
+      const entity = {
+        id: undefined,
+        name: event.object.properties.name,
+        lon: event.coordinate[0],
+        lat: event.coordinate[1]
+      };
+
+      if (event.object.properties.gtfsId) {
+        entity.lon = event.object.geometry.coordinates[0];
+        entity.lat = event.object.geometry.coordinates[1];
+        entity.id = event.object.properties.gtfsId;
+      } else if (event.object.properties.point === "vertibase") {
+        entity.lon = event.object.geometry.coordinates[0];
+        entity.lat = event.object.geometry.coordinates[1];
+        entity.id = `neom:line_${event.object.properties["ref:module"]}_vertibase`;
+      } else if (event.object.properties.level) {
+        entity.id = `neom:line_${event.object.properties["ref:module"]}_level_${event.object.properties["ref:level"]}`;
+      } else if (event.object.properties.module) {
+        entity.id = `neom:line_${event.object.properties["ref:module"]}_level_0`;
+      }
+
+      setClickedEntity(entity);
+    }
+  }, []);
+
+  function onViewStateChange({ viewState }) {
+    setFarOut(viewState.zoom <= 13);
+    setFromAbove(viewState.pitch <= 10);
+  }
+
+  function getCursor({ isHovering }) {
+    return isHovering ? "pointer" : "";
+  }
+
+  return (
+    <>
+      <DeckGLOverlay
+        id={id}
+        layers={layers}
+        interleaved
+        onClick={onClick}
+        onViewStateChange={onViewStateChange}
+        getCursor={getCursor}
+        alwaysShow={alwaysShow}
+        visible={visible}
+      />
+      {clickedEntity && (
+        <Popup
+          latitude={clickedEntity.lat}
+          longitude={clickedEntity.lon}
+          maxWidth="100%"
+          onClose={() => setClickedEntity(null)}
+        >
+          <EntityPopup
+            entity={{ ...clickedEntity, id: clickedEntity.id }}
+            setLocation={
+              setLocation
+                ? location => {
+                    setClickedEntity(null);
+                    setLocation(location);
+                  }
+                : null
+            }
+            setViewedStop={setViewedStop}
+          />
+        </Popup>
+      )}
+    </>
+  );
+}

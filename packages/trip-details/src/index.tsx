9f02350f6417dd75120c3c958b9e3d360127ce99
Zsombor Welker <zsombor.welker@realcity.io>
2023-06-04 14:28:26 +0200

    TripDetails: total distance, duration and average speed

--- packages/trip-details/src/index.tsx
+++ packages/trip-details/src/index.tsx
@@ -1,25 +1,13 @@
 import flatten from "flat";
-import coreUtils from "@opentripplanner/core-utils";
 import React, { ReactElement } from "react";
-import { FormattedMessage, FormattedNumber, useIntl } from "react-intl";
-// import { CalendarAlt } from "@styled-icons/fa-solid/CalendarAlt";
-import { Heartbeat } from "@styled-icons/fa-solid/Heartbeat";
-import { MoneyBillAlt } from "@styled-icons/fa-solid/MoneyBillAlt";
-// import { Leaf } from "@styled-icons/fa-solid/Leaf";
-import { Route } from "@styled-icons/fa-solid/Route";
-
-// import { Ruler } from "@styled-icons/fa-solid";
+import { FormattedMessage, useIntl } from "react-intl";
 import { humanizeDistanceString } from "@opentripplanner/humanize-distance";
 import * as S from "./styled";
 import TripDetail from "./trip-detail";
 import FareLegTable from "./fare-table";
-import { boldText, renderFare } from "./utils";
+import { boldText } from "./utils";
 
-import {
-  TimeActiveDetailsProps,
-  TransitFareProps,
-  TripDetailsProps
-} from "./types";
+import { TripDetailsProps } from "./types";
 
 // Load the default messages.
 import defaultEnglishMessages from "../i18n/en-US.yml";
@@ -30,225 +18,33 @@ import defaultEnglishMessages from "../i18n/en-US.yml";
 // - the yaml loader for jest returns messages with flattened ids.
 const defaultMessages: Record<string, string> = flatten(defaultEnglishMessages);
 
-function CO2DescriptionLink(contents: ReactElement): ReactElement {
-  return (
-    <a
-      href="https://www.itf-oecd.org/sites/default/files/life-cycle-assessment-calculations-2020.xlsx"
-      rel="noopener noreferrer"
-      target="_blank"
-    >
-      {contents}
-    </a>
-  );
-}
-/**
- * Default rendering if no component is provided for the TimeActiveDetails
- * slot in the TripDetails component.
- */
-function DefaultTimeActiveDetails({
-  bikeMinutes,
-  walkMinutes
-}: TimeActiveDetailsProps): ReactElement {
-  return (
-    <FormattedMessage
-      defaultMessage={
-        defaultMessages["otpUi.TripDetails.timeActiveDescription"]
-      }
-      description="Text describing the walking and biking durations of a trip."
-      id="otpUi.TripDetails.timeActiveDescription"
-      values={{
-        bikeMinutes,
-        strong: boldText,
-        walkMinutes
-      }}
-    />
-  );
-}
-
-/**
- * Helper component that renders a transit fare entry.
- */
-const TransitFare = ({
-  fareKey,
-  fareNameFallback,
-  fareKeyNameMap,
-  transitFares
-}: TransitFareProps): ReactElement => {
-  const currentFare = transitFares[fareKey];
-
-  // TODO: Is this needed? Every implementation of TransitFare does a check for currentFare's existence, although not the cents field
-  if (typeof currentFare?.cents !== "number") {
-    return (
-      <FormattedMessage
-        defaultMessage={defaultMessages["otpUi.TripDetails.transitFareUnknown"]}
-        description="Text showing that no fare information is present."
-        id="otpUi.TripDetails.transitFareUnknown"
-      />
-    );
-  }
-
-  return (
-    <span>
-      <FormattedMessage
-        defaultMessage={defaultMessages["otpUi.TripDetails.transitFareEntry"]}
-        description="Text showing the price of tickets on public transportation."
-        id="otpUi.TripDetails.transitFareEntry"
-        values={{
-          name: fareKeyNameMap[fareKey] || fareNameFallback || fareKey,
-          strong: boldText,
-          value: renderFare(
-            currentFare?.currency?.currencyCode || "USD",
-            currentFare.cents / 100
-          )
-        }}
-      />
-    </span>
-  );
-};
-
 /**
  * Renders trip details such as departure instructions, fare amount, and minutes active.
  */
 export function TripDetails({
   className = "",
-  co2Config,
-  defaultFareKey = "regular",
   DepartureDetails = null,
-  displayTimeActive = true,
-  FareDetails = null,
-  fareDetailsLayout,
-  fareKeyNameMap = {},
-  itinerary,
-  TimeActiveDetails = DefaultTimeActiveDetails
+  useMetricUnits,
+  itinerary
 }: TripDetailsProps): ReactElement {
-  // process the transit fare
-  const fareResult = coreUtils.itinerary.calculateTncFares(itinerary);
-  const { currencyCode, maxTNCFare, minTNCFare } = fareResult;
-  const transitFares = itinerary?.fare?.fare;
-
-  let companies = "";
-  itinerary.legs.forEach(leg => {
-    if (leg.tncData) {
-      companies = leg.tncData.company;
-    }
-  });
-  let fare;
-
-  const fareKeys = transitFares && Object.keys(transitFares).sort();
-
-  if (transitFares && fareKeys.length > 0) {
-    let defaultFare = defaultFareKey;
-    if (!transitFares[defaultFareKey]) {
-      defaultFare = "regular";
-    }
-
-    // Depending on if there are additional fares to display either render a <span> or a <details>
-    const TransitFareWrapper =
-      transitFares && fareKeys.length > 1 ? S.TransitFare : S.TransitFareSingle;
-
-    fare = transitFares?.[defaultFare] && (
-      <S.Fare>
-        <TransitFareWrapper>
-          <summary style={{ display: fareKeys.length > 1 ? "list-item" : "" }}>
-            <TransitFare
-              fareNameFallback={
-                <FormattedMessage
-                  defaultMessage={
-                    defaultMessages["otpUi.TripDetails.transitFare"]
-                  }
-                  description="Text showing the price of tickets on public transportation."
-                  id="otpUi.TripDetails.transitFare"
-                />
-              }
-              fareKey={defaultFare}
-              fareKeyNameMap={fareKeyNameMap}
-              transitFares={transitFares}
-            />
-          </summary>
-          {fareDetailsLayout ? (
-            // Show full ƒare details by leg
-            <FareLegTable layout={fareDetailsLayout} itinerary={itinerary} />
-          ) : (
-            // Just show the fares for each payment type
-            fareKeys.map(fareKey => {
-              // Don't show the default fare twice!
-              if (fareKey === defaultFare) {
-                return null;
-              }
-              return (
-                <TransitFare
-                  fareKey={fareKey}
-                  fareKeyNameMap={fareKeyNameMap}
-                  key={fareKey}
-                  transitFares={transitFares}
-                />
-              );
-            })
-          )}
-        </TransitFareWrapper>
-        {minTNCFare !== 0 && (
-          <S.TNCFare>
-            <br />
-            <FormattedMessage
-              defaultMessage={defaultMessages["otpUi.TripDetails.tncFare"]}
-              description="Text showing the price paid to transportation network companies."
-              id="otpUi.TripDetails.tncFare"
-              values={{
-                companies: (
-                  // S.TNCFareCompanies capitalizes the TNC company ID (e.g. "COMPANY")
-                  // after it is converted to lowercase, so it renders as "Company".
-                  <S.TNCFareCompanies>
-                    {companies.toLowerCase()}
-                  </S.TNCFareCompanies>
-                ),
-                maxTNCFare: renderFare(currencyCode, maxTNCFare),
-                minTNCFare: renderFare(currencyCode, minTNCFare),
-                strong: boldText
-              }}
-            />
-          </S.TNCFare>
-        )}
-      </S.Fare>
-    );
-  }
-
   const departureDate = new Date(itinerary.startTime);
 
-  // Compute total time spent active.
+  const intl = useIntl();
 
-  // TODO: separate into two reducers
-  let walkDurationSeconds = 0;
-  let bikeDurationSeconds = 0;
-  itinerary.legs.forEach(leg => {
-    if (leg.mode.startsWith("WALK")) walkDurationSeconds += leg.duration;
-    if (leg.mode.startsWith("BICYCLE")) bikeDurationSeconds += leg.duration;
-  });
-  const bikeMinutes = Math.round(bikeDurationSeconds / 60);
-  const walkMinutes = Math.round(walkDurationSeconds / 60);
-  const minutesActive = bikeMinutes + walkMinutes;
+  const totalDuration = itinerary.duration;
 
-  // Calculate CO₂ if it's not provided by the itinerary
-  const co2 =
-    itinerary.co2 ||
-    (co2Config?.enabled &&
-      coreUtils.itinerary.calculateEmissions(
-        itinerary,
-        co2Config?.carbonIntensity,
-        co2Config?.units
-      ));
-
-  // Parse flex info and generate appropriate strings
-  const containsFlex = itinerary.legs.some(coreUtils.itinerary.isFlex);
-  const pickupBookingInfo = itinerary.legs
-    .map(leg => leg.pickupBookingInfo)
-    .filter(info => !!info);
-  const dropOffBookingInfo = itinerary.legs
-    .map(leg => leg.dropOffBookingInfo)
-    .filter(info => !!info);
-  const totalDistance = itinerary.legs.reduce(
-    (total, leg) => total + leg.distance,
-    0
+  const totalTripDistance = itinerary.legs
+    .filter(leg => typeof leg.distance === "number")
+    .map(leg => leg.distance)
+    .reduce((prev, current) => prev + current, 0);
+  const formattedTotalTripDistance = humanizeDistanceString(
+    totalTripDistance,
+    useMetricUnits,
+    false,
+    intl
   );
+
+  const averageSpeed = Math.round((totalTripDistance / totalDuration) * 3.6);
 
   return (
     <S.TripDetails className={className}>
@@ -282,58 +78,60 @@ export function TripDetails({
             </S.Timing>
           }
         />
-
-        {fare && (
-          <TripDetail
-            // Any custom description for the transit fare needs to be handled by the slot.
-            description={
-              FareDetails && (
-                <FareDetails
-                  maxTNCFare={maxTNCFare}
-                  minTNCFare={minTNCFare}
-                  transitFares={transitFares}
-                />
-              )
-            }
-            summary={fare}
-          />
-        )}
-
-        {co2 > 0 && co2Config?.enabled && (
+        {totalTripDistance > 0 && (
           <TripDetail
             summary={
-              <S.CO2Summary>
+              <S.Timing className="trip-details-distance">
                 <FormattedMessage
-                  defaultMessage={defaultMessages["otpUi.TripDetails.co2"]}
-                  description="Text showing the quantity of CO₂ emitted by this trip."
-                  id="otpUi.TripDetails.co2"
+                  defaultMessage={
+                    defaultMessages["otpUi.TripDetails.totalDistance"]
+                  }
+                  description="Text showing the total distance for a trip."
+                  id="otpUi.TripDetails.totalDistance"
                   values={{
-                    co2: (
-                      <FormattedNumber
-                        value={Math.round(co2)}
-                        // eslint-disable-next-line react/style-prop-object
-                        style="unit"
-                        unit={co2Config?.units || "gram"}
-                        unitDisplay="narrow"
-                      />
-                    ),
+                    totalTripDistance: formattedTotalTripDistance,
                     strong: boldText
                   }}
                 />
-              </S.CO2Summary>
+              </S.Timing>
             }
-            description={
-              <FormattedMessage
-                defaultMessage={
-                  defaultMessages["otpUi.TripDetails.co2description"]
-                }
-                description="Text explaining how the CO₂ emissions are calculated."
-                id="otpUi.TripDetails.co2description"
-                values={{
-                  link: CO2DescriptionLink,
-                  totalDistance
-                }}
-              />
+          />
+        )}
+        {totalDuration > 0 && (
+          <TripDetail
+            summary={
+              <S.Timing className="trip-details-duration">
+                <FormattedMessage
+                  defaultMessage={
+                    defaultMessages["otpUi.TripDetails.totalDuration"]
+                  }
+                  description="Text showing the total duration for a trip."
+                  id="otpUi.TripDetails.totalDuration"
+                  values={{
+                    minutes: Math.round(totalDuration / 60),
+                    strong: boldText
+                  }}
+                />
+              </S.Timing>
+            }
+          />
+        )}
+        {averageSpeed > 0 && (
+          <TripDetail
+            summary={
+              <S.Timing className="trip-details-speed">
+                <FormattedMessage
+                  defaultMessage={
+                    defaultMessages["otpUi.TripDetails.averageSpeed"]
+                  }
+                  description="Text showing the average speed for a trip."
+                  id="otpUi.TripDetails.averageSpeed"
+                  values={{
+                    averageSpeedInKmh: averageSpeed,
+                    strong: boldText
+                  }}
+                />
+              </S.Timing>
             }
           />
         )}
